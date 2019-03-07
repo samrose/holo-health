eb0bb1e05d46f4a2d69177b09112ac19650ff329
Sam Rose <samuel.rose@gmail.com>
2019-03-07 19:18:47 +0000

    consolidate

--- health.go
+++ health.go
@@ -6,8 +6,36 @@ package main
     import "github.com/jacobsa/go-serial/serial"
     import "github.com/ssimunic/gosensors"
     import "github.com/kolide/osquery-go"
-func main(){
 
+func read_sensors(){
+	sensors, err := gosensors.NewFromSystem()
+	// sensors, err := gosensors.NewFromFile("/path/to/log.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Sensors implements Stringer interface,
+	// so code below will print out JSON
+	fmt.Println(sensors)
+
+	// Also valid
+	// fmt.Println("JSON:", sensors.JSON())
+
+	// Iterate over chips
+	for chip := range sensors.Chips {
+		// Iterate over entries
+		for key, value := range sensors.Chips[chip] {
+			// If CPU or GPU, print out
+			if key == "CPU" || key == "GPU" {
+				fmt.Println(key, value)
+			}
+		}
+	}
+
+}
+
+func flash_red(){
     // Set up options.
     options := serial.OpenOptions{
       PortName: "/dev/ttyUSB0",
@@ -26,13 +54,18 @@ func main(){
     // Make sure to close it later.
     defer port.Close()
 
-    // Write 4 bytes to the port.
-    b := []byte("R*")
+    // Write 2 bytes to the port.
+    b := []byte("P*")
     n, err := port.Write(b)
     if err != nil {
       log.Fatalf("port.Write: %v", err)
     }
 
     fmt.Println("Wrote", n, "bytes.")
+
+}
+func main(){
+    read_sensors()
+    flash_red()
 }
 
